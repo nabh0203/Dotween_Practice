a00898c07b5ad2a0ba377bfb1a3cc78aab9c5a76
Nabyeongho <127078321+nabh0203@users.noreply.github.com>
2024-06-20 14:51:29 +0900

    DOTWEEN 기능
    DOScale , DOMove

--- DotweenTest/Assets/Scripts/TestCube.cs
+++ DotweenTest/Assets/Scripts/TestCube.cs
@@ -5,10 +5,30 @@ using DG.Tweening;
 
 public class TestCube : MonoBehaviour
 {
+    Vector3 targetPos = new Vector3(0, 5, 0);
+
     void Start()
     {
         //transform.DOShakePosition(10f);    
         var targetScale = new Vector3(2, 3, 2);
-        transform.DOScale(targetScale, 3f);
+        //transform.DOScale(targetScale, 3f);
+        //처음부터 반복
+        //transform.DOScale(targetScale, 3f).SetLoops(3,LoopType.Restart);
+        //반복마다 증가
+        //transform.DOScale(targetScale, 3f).SetLoops(3, LoopType.Incremental);
+        //왕복
+        //transform.DOScale(targetScale, 3f).SetLoops(3, LoopType.Yoyo);
+
+        //SetDelay(2f); 를 사용하여 2초뒤에 크기 증가
+        //transform.DOScale(targetScale, 3f).SetDelay(2f);
+
+        //Ease 값을 사용하여 크기 증가
+        //transform.DOScale(targetScale, 3f).SetEase(Ease.InCirc);
+        //통통 튀면서 증가
+        //transform.DOScale(targetScale, 3f).SetEase(Ease.InOutBounce);
+
+        //OnComplete 을 사용하여 앞에 식이 끝나면 후에 있는 식을 실행한다.
+        transform.DOScale(targetScale, 3f).SetEase(Ease.InOutBounce).OnComplete(() => transform.DOMove(targetPos,2f));
+        
     }
 }
