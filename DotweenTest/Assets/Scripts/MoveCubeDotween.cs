bc0b9ea7a6a926d3f26bd70ffeaa4c6f1bfda1bd
Nabyeongho <127078321+nabh0203@users.noreply.github.com>
2024-06-20 15:15:36 +0900

    DOTWEEN 기능 3
    Loop , StopMotion

--- DotweenTest/Assets/Scripts/MoveCubeDotween.cs
+++ DotweenTest/Assets/Scripts/MoveCubeDotween.cs
@@ -7,9 +7,12 @@ public class MoveCubeDotween : MonoBehaviour
 {
     //목표 위치 값
     Vector3 targetPos = new Vector3(0, 5, 0);
-
+    public Ease ease;
     private void Start()
     {
-        transform.DOMove(targetPos, 1f);
+        //.SetLoops(-1,LoopType.Restart) 로 하면 무한 루프가 된다.
+        //transform.DOMove(targetPos, 1f).SetEase(ease).SetLoops(-1,LoopType.Restart);
+        //StopMotion 을 사용하면 프레임 단위로 끊기면서 움직인다.
+        transform.DOMove(targetPos, 1f).SetEase(EaseFactory.StopMotion(10,Ease.Linear));
     }
 }
